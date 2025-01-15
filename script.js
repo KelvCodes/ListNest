03d35f89ea92f4c0bce5ed0c4038e4ef9b7cfb18
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-01-15 12:37:06 -0800

    Update script.js

--- script.js
+++ script.js
@@ -97,7 +97,7 @@ function getCurrentTime() {
 
     // Convert to 12-hour format
     hours = hours % 12;
-    hours = hours ? hours : 12; // the hour '0' should be '12'
+    hours = hours ? hours : 12; // the hour '0' should be '12'.
     minutes = minutes < 10 ? '0' + minutes : minutes;
 
     return `${hours}:${minutes} ${ampm}`;
